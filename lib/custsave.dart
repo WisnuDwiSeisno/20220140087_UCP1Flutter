0af5fb55423e6243850b1fe806368193244d96b2
WisnuDwiSeisno <wseisno37@gmail.com>
2025-04-29 21:05:21 +0700

    membuat detail customer

---
+++ lib/custsave.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1/datapelanggan.dart';
+import 'package:ucp1/home.dart';
+
+class CustSavePage extends StatelessWidget {
+  final Customer customer;
+
+  const CustSavePage({super.key, required this.customer});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFEF8F6),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 0, 162, 255),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white, size: 30),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: Text(
+          'Detail ${customer.nama}',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('images/logo.png', width: 120, height: 120),
+              SizedBox(height: 20),
+              Text(customer.nama, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              SizedBox(height: 6),
+              Text(customer.email, style: TextStyle(fontSize: 12, color: const Color.fromARGB(255, 0, 0, 0))),
+              SizedBox(height: 6),
+              Text(customer.nohp, style: TextStyle(fontSize: 16, color: const Color.fromARGB(255, 0, 0, 0))),
+              SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Alamat',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: TextEditingController(text: customer.alamat),
+                    decoration: InputDecoration(
+                      labelText: 'Alamat',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Alamat tidak boleh kosong!';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Kode Pos',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            TextFormField(
+                              keyboardType: TextInputType.number,
+                              controller: TextEditingController(text: customer.kode),
+                              decoration: InputDecoration(
+                                labelText: 'Kode Pos',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Kode Pos tidak boleh kosong!';
+                                }
+                                return null;
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Provinsi',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            TextFormField(
+                              keyboardType: TextInputType.text,
+                              controller: TextEditingController(
+                                text: customer.provinsi,
+                              ),
+                              decoration: InputDecoration(
+                                labelText: 'Provinsi',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Provinsi tidak boleh kosong!';
+                                }
+                                return null;
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  
+                  SizedBox(height: 50),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 0, 162, 255),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        );
+                      },
+                      child: Text(
+                        'Selesai',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
