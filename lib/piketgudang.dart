cefa2322106fa3ae709e314991b46e6a69da6c98
WisnuDwiSeisno <wseisno37@gmail.com>
2025-04-29 17:23:07 +0700

    membuat appbar

--- lib/piketgudang.dart
+++ lib/piketgudang.dart
@@ -10,6 +10,28 @@ class PiketPage extends StatefulWidget {
 class _PiketPageState extends State<PiketPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 0, 162, 255),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white, size: 35,),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          'Piket Gudang',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Center(child: Text('Piket Gudang Page')),
+    );
   }
 }
