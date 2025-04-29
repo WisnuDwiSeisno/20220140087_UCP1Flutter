85e2c27b696dcb670d315de985602ab5b33bb119
WisnuDwiSeisno <wseisno37@gmail.com>
2025-04-29 15:53:42 +0700

    membuat logout button

--- lib/home.dart
+++ lib/home.dart
@@ -12,41 +12,47 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(child: 
-      Column(
-        children: [
-          Container(
-            color: Color.fromARGB(255, 0, 162, 255),
-            padding: EdgeInsets.all(16),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundImage: AssetImage('images/logo.png'),
-                ),
-                SizedBox(width: 16,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Selamat Datang',
-                      style: TextStyle(color: Colors.white, fontSize: 14),
-                    ),
-                    Text(
-                      'Admin',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              color: Color.fromARGB(255, 0, 162, 255),
+              padding: EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('images/logo.png'),
+                  ),
+                  SizedBox(width: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Selamat Datang',
+                        style: TextStyle(color: Colors.white, fontSize: 14),
                       ),
-                    ),
-                  ],
-                )
-              ],
+                      Text(
+                        'Admin',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.logout, color: Colors.white),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
-      )),
+          ],
+        ),
+      ),
     );
   }
 }
