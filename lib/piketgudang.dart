26d1c3c06fca8ff8a64d13134a4025f1e6d8c882
WisnuDwiSeisno <wseisno37@gmail.com>
2025-04-29 18:10:03 +0700

    membuat nama anggota dan tanggal

--- lib/piketgudang.dart
+++ lib/piketgudang.dart
@@ -8,14 +8,25 @@ class PiketPage extends StatefulWidget {
 }
 
 class _PiketPageState extends State<PiketPage> {
+  final TextEditingController namaController = TextEditingController(
+    text: 'Admin',
+  );
+  final TextEditingController tanggalController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 0, 162, 255),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white, size: 35,),
+          icon: Icon(Icons.arrow_back, color: Colors.white, size: 35),
           onPressed: () {
             Navigator.pop(context);
           },
@@ -31,7 +42,79 @@ class _PiketPageState extends State<PiketPage> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Center(child: Text('Piket Gudang Page')),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Nama Anggota',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  controller: namaController,
+                  decoration: InputDecoration(
+                    labelText: 'Nama Anggota',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email tidak boleh kosong!';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Pilih Tanggal',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8),
+                TextFormField(
+                  controller: tanggalController,
+                  readOnly: true,
+                  onTap: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2101),
+                    );
+                    if (pickedDate != null) {
+                      setState(() {
+                        tanggalController.text =
+                            "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+                      });
+                    }
+                  },
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.calendar_today),
+                    labelText: 'Pilih Tanggal',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email tidak boleh kosong!';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
